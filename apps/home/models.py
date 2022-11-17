b08aaf8b9c8daa44bb9f1ee58e488db39a458edd
alliswell <31767534+amruthakrishnap@users.noreply.github.com>
2022-11-17 18:29:18 +0000

    hello

--- apps/home/models.py
+++ apps/home/models.py
@@ -21,19 +21,19 @@ class UserDetails(models.Model):
 # INSERTING Data
 Default_Data = [ 
     (56, "shareadmin", "tester@gmail.com", "367238", 
-    "{'overall': {'New Viewers': '178', 'Views': '9.5M', 'followers': '131k', 'New Views pct': '60', 'Views Pct': '92', 'follow pct': '89'}, 'Analytics':{'TP': '15', 'TWH': '0.20', 'WL': [8, 2, 9, 5, 4, 6, 4, 3, 8, 0]}}"),
+    "{'overall': {'New Viewers': '17K', 'Views': '9.5M', 'followers': '131k', 'New Views pct': '80', 'Views Pct': '92', 'follow pct': '89'}, 'Analytics':{'TP': '5301', 'TWH': '203.2', 'WL': [8, 2, 9, 5, 4, 6, 4, 3, 8, 0]}}"),
     
     (25, "Tester", "tester@gmail.com", "367238", 
-    "{'overall':{'New Viewers': '10', 'Views': '3.6k', 'followers': '14', 'New Views pct': '60', 'Views Pct': '92', 'follow pct': '89'}, 'Analytics':{'TP': '15', 'TWH': '0.20', 'WL': [8, 2, 9, 5, 4, 6, 4, 3, 8, 0]}}"),
+    "{'overall':{'New Viewers': '700', 'Views': '3.6k', 'followers': '14', 'New Views pct': '60', 'Views Pct': '92', 'follow pct': '45'}, 'Analytics':{'TP': '68', 'TWH': '0.36', 'WL': [8, 2, 9, 5, 4, 6, 4, 3, 8, 0]}}"),
 
     (895, "banker", "baker@gmail.com", "367238", 
-    "{'overall':{'New Viewers': '25', 'Views': '2.1k', 'followers': '67', 'New Views pct': '29', 'Views Pct': '38', 'follow pct': '67'}, 'Analytics':{'TP': '190', 'TWH': '20', 'WL': [1, 9, 10, 8, 10, 2, 4, 3, 8, 0]}}"),
+    "{'overall':{'New Viewers': '7K', 'Views': '204k', 'followers': '23.3K', 'New Views pct': '65', 'Views Pct': '78', 'follow pct': '67'}, 'Analytics':{'TP': '190', 'TWH': '2.3', 'WL': [1, 9, 10, 8, 10, 2, 4, 3, 8, 0]}}"),
 
     (35, "Easeter", "easter@gmail.com", "989137827", 
-    "{'overall':{'New Viewers': '1230', 'Views': '9.8M', 'followers': '58909'}, 'New Views pct': '60', 'Views Pct': '92', 'follow pct': '89'},'Analytics':{'TP': '15', 'TWH': '0.20', 'WL': [8, 2, 9, 5, 4, 6, 4, 3, 8, 0]}}"),
+    "{'overall':{'New Viewers': '12K', 'Views': '9.8M', 'followers': '58909'}, 'New Views pct': '60', 'Views Pct': '92', 'follow pct': '89'},'Analytics':{'TP': '6800', 'TWH': '6.7', 'WL': [8, 2, 9, 5, 4, 6, 4, 3, 8, 0]}}"),
 
     (165, "Broker", "broker@gmail.com", "112337827", 
-    "{'overall':{'New Viewers': '120', 'Views': '3.6M', 'followers': '1400'}, 'New Views pct': '60', 'Views Pct': '92', 'follow pct': '89'},'Analytics':{'TP': '15', 'TWH': '0.20', 'WL': [8, 2, 9, 5, 4, 6, 4, 3, 8, 0]}}"),
+    "{'overall':{'New Viewers': '28K', 'Views': '3.6M', 'followers': '14K'}, 'New Views pct': '60', 'Views Pct': '92', 'follow pct': '80'},'Analytics':{'TP': '2022', 'TWH': '9.8', 'WL': [8, 2, 9, 5, 4, 6, 4, 3, 8, 0]}}"),
 ]
 
 for row in Default_Data:
